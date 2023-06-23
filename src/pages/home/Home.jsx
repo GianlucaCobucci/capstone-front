6502789d855600f199dd169e59277e32bbb640d5
GianlucaCobucci <gianlucacobucci330@gmail.com>
2023-06-23 14:15:24 +0200

    added like function with counter

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import Topbar from '../../components/topbar/Topbar'
-import Sidebar from '../../components/sidebar/Sidebar'
-import Feed from '../../components/feed/Feed'
-import Rightbar from '../../components/rightbar/Rightbar'
-import './home.css'
+import React from "react";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import "./home.css";
 
 const Home = () => {
   return (
@@ -15,7 +15,7 @@ const Home = () => {
           <Rightbar />
         </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
