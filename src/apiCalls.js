6d204aa22cc64dc0cad741b0626c1938365f1abd
GianlucaCobucci <gianlucacobucci330@gmail.com>
2023-07-13 21:32:55 +0200

    fetched users conversations between friends

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -6,8 +6,8 @@ export const loginCall = async (userCredential, dispatch) => {
     try {
         const res = await axios.post("auth/login", userCredential)
         dispatch({type: "LOGIN_SUCCESS", payload: res.data})
+        return res.data; // ritornare i dati dell'utente
     } catch (error)  {
         dispatch({type: "LOGIN_FAILURE", payload: error})
-
     }
 }
