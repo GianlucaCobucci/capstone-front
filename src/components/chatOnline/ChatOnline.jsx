da645ef67ddc81fffd9cb7ac39293482fc2865d0
GianlucaCobucci <gianlucacobucci330@gmail.com>
2023-07-14 12:22:46 +0200

    adjusted chat

--- src/components/chatOnline/ChatOnline.jsx
+++ src/components/chatOnline/ChatOnline.jsx
@@ -1,26 +1,58 @@
-import React from 'react'
-import './chatOnline.css'
+import React, { useState, useEffect } from "react";
+import "./chatOnline.css";
+import axios from "axios";
+import avatar from "../../assets/noAvatar.jpeg";
+import { nanoid } from "nanoid";
 
-const ChatOnline = () => {
-  return (
-    <div className='chatOnline'>
-        <div className="chatOnlineFriend">
-            <div className="chatOnlineImgContainer">
-                <img className='chatOnlineImg'
-                    src="https://picsum.photos/50/50" 
-                    alt=""
-                />
 
-                <div className="chatOnlineBadge">
+const ChatOnline = ({ onlineUsers, currentId, setCurrentChat }) => {
+    const [friends, setFriends] = useState([]);
+    //console.log(friends);
 
+    const [onlineFriends, setOnlineFriends] = useState([]);
+
+    useEffect(() => {
+        const getFriends = async () => {
+            const res = await axios.get(`/users/friends/${currentId}`);
+            setFriends(res.data.friendList);
+        };
+        getFriends();
+    }, [currentId]);
+
+    useEffect(() => {
+        setOnlineFriends(
+            friends.filter((friend) => onlineUsers.includes(friend._id))
+        );
+    }, [friends, onlineUsers]);
+    //console.log(onlineUsers)
+
+    const handleClick = async (user) => {
+        try {
+            const res = await axios.get(`/conversations/find/${currentId}/${user._id}`)
+            setCurrentChat(res.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <div className="chatOnline">
+            {onlineFriends.map((online) => (
+                <div key={nanoid()} className="chatOnlineFriend" onClick={() => handleClick(online)}>
+                    <div className="chatOnlineImgContainer">
+                        <img
+                            className="chatOnlineImg"
+                            src={online?.profilePicture || "https://picsum.photos/50/50" || avatar}
+                            alt=""
+                        />
+
+                        <div className="chatOnlineBadge"></div>
+                    </div>
+                    <span className="chatOnlineName">{online?.username}</span>
                 </div>
-            </div>
-            <span className="chatOnlineName">
-                Mario Rossi
-            </span>
+            ))}
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default ChatOnline
+export default ChatOnline;
